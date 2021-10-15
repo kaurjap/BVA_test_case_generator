49deef15f333cd91a19cb66aa5fc04c1bb741918
Japneet Kaur <japneetkaur@Japneets-MacBook-Pro.local>
2021-10-15 19:57:32 -0400

    first files

---
+++ Main.java
@@ -0,0 +1,27 @@
+
+/**
+ * Main class
+ * 
+ * Includes the beginning point of the program, and is the class that interacts directly with the user 
+ */
+public class Main {
+
+    public static void main (String[] args) {
+
+        // ask for type of testing
+
+
+    } // end main
+
+    /**
+     * method that asks the user for the type of testing to perform
+     * @return 
+     *      0   int    Boundary Value Testing
+     *      1   int    Robustness Testing
+     *      -1  int    if they
+     */
+    public int getTestingType () {
+
+    } // end getTestingType
+
+} // end class Main
